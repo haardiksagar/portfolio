2e52aef1742ef16a605a16d5a2459b425bf86a5d
Haardik Sagar <haardiksagar@gmail.com>
2026-07-09 10:44:51 +0530

    preparing for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export', // Forces Next.js to generate a static HTML/CSS/JS "out" folder
+  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+  images: {
+    unoptimized: true, // GitHub Pages does not support Next.js default image optimization
+  },
 };
 
 export default nextConfig;
