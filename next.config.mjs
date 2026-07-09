ec68058aceb6ee59471773e565eaa86d59073ff7
Haardik Sagar <haardiksagar@gmail.com>
2026-07-09 10:55:03 +0530

    updated the base path in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Forces Next.js to generate a static HTML/CSS/JS "out" folder
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+  //basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
   images: {
     unoptimized: true, // GitHub Pages does not support Next.js default image optimization
   },
